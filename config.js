a357a57a978b67cbc2d3336d95122309eb80d267
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-11 11:52:59 +0000

    Change webhook URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT || 4321,
-  SLACK_URL: 'https://hooks.slack.com/services/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',
+  SLACK_URL: 'https://hooks.slack.com/services/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',
   HOST: process.env.HOST || 'https://squixy.fwd.wf'
 }
