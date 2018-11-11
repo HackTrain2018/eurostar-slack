86e1a26466d7524a4f1028ba4ac18f66f3fa9209
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-11 06:16:32 +0100

    Provide Firebase configuration

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT || 4321
+  PORT: process.env.PORT || 4321,
+  SLACK_URL: "https://hooks.slack.com/services/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"
 }
